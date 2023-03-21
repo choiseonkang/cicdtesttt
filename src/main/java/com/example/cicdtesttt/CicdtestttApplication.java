61a09887df876e209e5aacfbc384c023aa7f1423
최선강 <choiseonkang@choeseongang-ui-MacBookPro.local>
2023-03-21 17:44:55 +0900

    Initial commit

---
+++ src/main/java/com/example/cicdtesttt/CicdtestttApplication.java
@@ -0,0 +1,13 @@
+package com.example.cicdtesttt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CicdtestttApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CicdtestttApplication.class, args);
+    }
+
+}
